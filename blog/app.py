5b2810f4870cd98cfb6851e9b03946fc20818cb8
rpersing <rpersing@mail.umw.edu>
2022-05-18 02:58:46 -0400

    Restructuring and cleanup

--- blog/app.py
+++ blog/app.py
@@ -1,1 +1,65 @@
-from app import app
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+import secrets
+
+app = Flask(__name__)
+secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = secret_key
+Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=30)])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(message='Invalid email'), Length(min=3, max=320)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=30)])
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', page="Home")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', page="Login", form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('register.html', page="Register", form=form)
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', page="Blog")
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html', page="Portfolio")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- blog/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'gygyGHYUG696%TR&&gi%t576gi&$G#58$65ifUHGIKG*&YY&OTF%&&#W@$WDFygyg7yO7gtyf56R%#&'
